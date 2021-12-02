f6bdb7d97b68a4355d2514397d19aa609d4bf043
Jung Kyung Jin <hijkg2@naver.com>
2021-12-02 15:58:54 +0900

    working

--- resources/js/ui.js
+++ resources/js/ui.js
@@ -1,7 +1,32 @@
 const ui = {
     init : function(){
-        console.log('실행');
+        if( $('.animate_wrap').length > 0 ) { this.circle.init() }
     },
+    circle : {
+        init : function(){
+            let _this = this,
+                $target = $('.animate_wrap'),
+                sec2T = $('section.sec02').offset().top,
+                sec3T = $('section.sec02').offset().top,
+                scT;
+
+            $(window).on('scroll', function(){
+                scT = $(this).scrollTop();
+                _this.evtHandler($target, scT, sec2T, sec3T);
+            })
+        },
+        evtHandler : function($target, scT, sec2T, sec2Tmat, sec3T, sec3Tmat){
+            if( scT <= sec2T ){
+                let rotMat = parseInt((100 - ((sec2T - scT) / sec2T * 100)) * 3.6);
+                $target.css({'transform':'translate(-50%, -50%) rotate('+rotMat+'deg)'})
+            } else if ( sct <= sec3T ) {
+                let pagMat = parseInt(100 - ((sec3T - scT) / sec3T * 100));
+                
+            } else {
+                $target.css({'transform':'translate(-50%, -50%) rotate(360deg)'})
+            };
+        }
+    }
 }
 
 $(document).ready(function(){
