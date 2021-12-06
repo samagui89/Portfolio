513cd0ce5a136b56e847302c391c54dc4a006d6b
Jung Kyung Jin <hijkg2@naver.com>
2021-12-06 10:30:42 +0900

    working

--- resources/js/ui.js
+++ resources/js/ui.js
@@ -1,64 +1,80 @@
+const tl = gsap.timeline();
 const ui = {
-    init : function(){
-        if( $('.animate_wrap').length > 0 ) { this.circle.init() }  // Scroll Evt
-        if( $('.visual_wrap').length > 0 ) { this.mouse.init() }    // Mouse Over Evt
-    },
-    circle : {
-        init : function(){
-            let _this = this,
-                $target = $('.animate_wrap'),
-                $btn = $target.find('.btn_main_ment'),
-                sec2T = $('section.sec02').offset().top,
-                sec3T = $('section.sec03').offset().top,
-                scT;
+	init : function(){
+		if( $('.animate_wrap').length > 0 ) { this.circle.init() }  // Scroll Evt
+		if( $('.visual_wrap').length > 0 ) { this.mouse.init() }	// Mouse Over Evt
+	},
+	circle : {
+		init : function(){
+			let _this = this,
+				$target = $('.animate_wrap'),
+				$btn = $target.find('.btn_main_ment'),
+				$cAll = $target.find('.circle_box > div'),
+				sec2T = $('section.sec02').offset().top,
+				sec3T = $('section.sec03').offset().top,
+				scT;
 
-            $(window).on('scroll', function(){
-                scT = $(this).scrollTop();
-                _this.evtHandler($target, $btn, scT, sec2T, sec3T);
-            })
-        },
-        evtHandler : function($target, $btn, scT, sec2T, sec3T){
-            if( scT <= sec2T ){
-                let rotMat = (sec2T - scT) / sec2T,
-                    rotParese = parseInt((100 - rotMat * 100) * 3.6);
-                $target.css({'transform':'translate(-50%, -50%) rotate('+ rotParese +'deg)'})
-                $btn.css({'transform':'scale('+rotMat.toFixed(1)+')'});
-            } else if ( scT <= sec3T ) {
-                $target.css({'transform':'translate(-50%, -50%) rotate(360deg)'})
-                $btn.css({'transform':'scale(0)'});
+			$(window).on('scroll', function(){
+				scT = $(this).scrollTop();
+				_this.evtHandler($target, $btn, $cAll, scT, sec2T, sec3T);
+			})
+		},
+		evtHandler : function($target, $btn, $cAll, scT, sec2T, sec3T){
+			if( scT <= sec2T ){
+				let rotMat = (sec2T - scT) / sec2T,
+					rotParese = parseInt((100 - rotMat * 100) * 3.6);
+				$target.css({'transform':'translate(-50%, -50%) rotate('+ rotParese +'deg)'})
+				$btn.css({'transform':'scale('+rotMat.toFixed(1)+')'});
+				$cAll.css({'font-size':'0px'});
+			} else if ( scT <= sec3T ) {
+				$target.css({'transform':'translate(-50%, -50%) rotate(360deg)'})
+				$btn.css({'transform':'scale(0)'});
 
-                let pagMat = parseInt(100 - ((sec3T - sec2T - scT) / sec3T * 100));
+				let pagMat = parseInt(100 - ((sec3T - sec2T - scT) / sec3T * 100)),
+					harfSec3T = ((sec3T - sec2T) / 2);
+					harfMat = parseInt(100 - ((harfSec3T - scT) / (harfSec3T - sec2T) * 100)) / 100;
+				if( harfMat <= 1 ){
+					$cAll.css({'font-size':harfMat * 24 + 'px'});
+					$cAll.closest('.circle_box').removeClass('active');
+				} else {
+					console.log(pagMat);
+					$cAll.css({'font-size':'24px'});
+					$cAll.closest('.circle_box').addClass('active');
+				}
+			} else {
+				$target.css({'transform':'translate(-50%, -50%) rotate(360deg)'})
+			};
+		}
+	},
+	mouse : {
+		init : function(){
+			let _this = this
+				$target = $('.visual_bg');
 
-                console.log(pagMat);
-            } else {
-                $target.css({'transform':'translate(-50%, -50%) rotate(360deg)'})
-            };
-        }
-    },
-    mouse : {
-        init : function(){
-            let _this = this,
-                $target = $('.visual01, .visual02');
+			_this.evtHandler($target);
+		},
+		evtHandler : function($target){
+			$target.on('mousemove', function(e){
+				let body = $('.visual_bg');
+				let circle = document.createElement('span');
+				let x = e.offsetX;
+				let y = e.offsetY;
+				circle.style.left = x + "px";
+				circle.style.top = y + "px";
 
-                _this.evtHandler($target);
-            
-        },
-        evtHandler : function($target){
-            $target.on('mouseenter', function(e){
-                console.log('들어오다'+e)
-            })
-            
-            $target.on('mousemove', function(e){
-                console.log('X좌표 : '+e.pageX+', Y좌표'+e.pageY)
-            })
+				let size = Math.random() * 100;
+				circle.style.width = 20 + size + "px";
+				circle.style.height = 20 + size + "px";
 
-            $target.on('mouseleave', function(e){
-                console.log('나가다'+e)
-            })
-        }
-    }
+				body.append(circle);
+				setTimeout(function() {
+					circle.remove();
+				}, 1800);
+			})
+		}
+	}
 }
 
 $(document).ready(function(){
-    ui.init();
+	ui.init();
 })
